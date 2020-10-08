69701462057c3aa245275cbb28f31ac660b8f898
conservativ007 <lexxar_89@mail.ru>
2020-10-09 01:37:24 +0300

    feat: added task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let regexpOne = /\(\)|\[\]|\{\}|\|\|/g;
+	let regexpFive = /12|34|56|77|88/g;
+
+	for(let i = str.length; i > 0; i--){
+		str = str.replace(regexpOne, '');
+		str = str.replace(regexpFive, '');
+	}
+
+	return str.length == 0 ? true : false;
 }
